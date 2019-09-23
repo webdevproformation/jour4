7dcbdeebceaad3e52b74cdec1bcba72f35b1ad1d
malik <malik.h@webdevpro.net>
2019-09-23 12:57:42 +0200

    pagination partie 2

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ArticlesLocalService } from '../service/articles-local.service';
+import { ActivatedRoute } from "@angular/router" ;
 
 @Component({
   selector: 'app-homepage',
@@ -12,10 +13,10 @@ export class HomepageComponent implements OnInit {
   nbArticleParPage = 2 
 
 
-  constructor( service : ArticlesLocalService) {
-
-    this.articles = service.getAll();
-   }
+  constructor( 
+      private service : ArticlesLocalService , 
+      private route : ActivatedRoute) 
+  {   }
 
   nbPage()
   {
@@ -23,6 +24,21 @@ export class HomepageComponent implements OnInit {
   }
 
   ngOnInit() {
+    //this.route.paramMap.subscribe( (params) => {  params })
+    this.route.queryParams.subscribe( (query) => { 
+      //console.log(query.get("page"));
+       if( Object.keys(query).length !== 0)
+      {
+        // affiche moi les 2 premiers articles
+        this.articles = this.service.getAll();
+      }
+      else (query.page)
+      {
+        const numPage = query.page ;
+        this.articles = this.service.getAll();
+        // afficher les deux articles de la page numPage
+      }
+    } )
   }
 
 }
