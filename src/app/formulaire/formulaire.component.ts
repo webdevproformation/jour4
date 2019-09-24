825c2a3b6bac6ac3f75fbda90c41c978b5a1cdbd
malik <malik.h@webdevpro.net>
2019-09-24 13:15:26 +0200

    fin nouveau formulaire

--- src/app/formulaire/formulaire.component.ts
+++ src/app/formulaire/formulaire.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormulaireComponent implements OnInit {
 
+  listePays = [
+    "France",
+    "Allemagne",
+    "Italie"
+  ]
+
   constructor() { }
 
   ngOnInit() {
